b01ecf951c4efcab69aecd2ac8ace4fe07f9faf2
TechnoTurtle777 <techno.turtle777@gmail.com>
2022-01-28 15:02:41 +0100

    refactored BookServiceImpl

--- backend/src/main/java/ch/zuehlke/fullstack/data/mapper/BookMapper.java
+++ backend/src/main/java/ch/zuehlke/fullstack/data/mapper/BookMapper.java
@@ -4,9 +4,12 @@ import ch.zuehlke.fullstack.data.model.Book;
 import ch.zuehlke.fullstack.domain.model.BookDto;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(
         componentModel = "spring"
 )
 public interface BookMapper {
     BookDto bookToBookDtoMapper(Book book);
+    List<BookDto> booklistToBookDtoListMapper(List<Book> books);
 }
--- backend/src/main/java/ch/zuehlke/fullstack/domain/service/BookServiceImpl.java
+++ backend/src/main/java/ch/zuehlke/fullstack/domain/service/BookServiceImpl.java
@@ -1,32 +1,30 @@
 package ch.zuehlke.fullstack.domain.service;
 
+import ch.zuehlke.fullstack.data.mapper.BookMapper;
+import ch.zuehlke.fullstack.data.repository.BookRepository;
 import ch.zuehlke.fullstack.domain.model.BookDto;
-import ch.zuehlke.fullstack.helper.BookHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.List;
 
 @Service
 @Profile("default")
 public class BookServiceImpl implements BookService {
 
+    @Autowired
+    private final BookRepository bookRepository;
+    private final BookMapper mapper;
+
+    public BookServiceImpl(BookRepository bookRepository, BookMapper mapper) {
+        this.bookRepository = bookRepository;
+        this.mapper = mapper;
+    }
+
     @Override
     public List<BookDto> getAllBooks() {
-        List<BookDto> booklist = new ArrayList<>();
-        BookDto dummyBookDto = new BookDto(1 ,"978-1-4920-7698-8",
-                "Spring Boot: Up and running",
-                new ArrayList<String>(Arrays.asList("Martin Heckler")),
-                BookHelper.stringToDate("01.01.2020"),
-                "Building Cloud Native Java and Kotlin Applications" ,
-                Double.valueOf(5),
-                new HashMap<String, String>(Map.of("https://assets.thalia.media/img/artikel/fce0f87046dcbf48cb73e0d96eb8e758c2a31ab0-00-00.jpeg","Cover")),
-                "Spring Boot is the most widely used Java framework available. Its ease and power have revolutionized application development from monoliths to microservices. Yet Spring Boots simplicity can also be confounding. This practical book shows you how to use this framework to write successful mission-critical applications.");
-
-        booklist.add(dummyBookDto);
-        return booklist;
+        return mapper.booklistToBookDtoListMapper(bookRepository.findAll());
     }
 
 }
